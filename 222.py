e74faa896b2cd71b43d73e2b0016adaddab5f55c
alenadidora <a.didora@g.nsu.ru>
2026-09-17 00:05:55 +0700

    13 commit

--- 222.py
+++ 222.py
@@ -1,4 +1,11 @@
-x = float(input())
-print(x - 4)
+cm = float(input())
+dume = cm / 2.54
+feet = dume / 12
+yards = feet / 3
+miles = yards / 1760
+print(f"{yards:.1f} ярдов")
+print(f"{miles:.1f} мили")
+print(f"{feet:.1f} футов")
+print(f"{dume:.1f} дюймов")
 
 
