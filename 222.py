0f08c4ab193bef7c7713c14a0c1c6ed455402283
alenadidora <a.didora@g.nsu.ru>
2026-09-17 00:04:39 +0700

    10 commit

--- 222.py
+++ 222.py
@@ -1,7 +1,9 @@
-num = input()
-name1 = input()
-name2 = input()
-city1 = input()
-city2 = input()
-print('Заканчивается посадка на рейс', num, name1, 'до', city1)
-print('This is the final boarding call for', name2, 'flight', num, 'to', city2)
+cost = float(input())
+a = 96
+b = 48
+gold = a // 16
+total = a * b
+gold_total = cost - total
+price = gold_total / gold
+print(price)
+
