334858a1554efc9820a08f08d500ba5153b4582d
alenadidora <a.didora@g.nsu.ru>
2026-09-17 00:06:45 +0700

    15 commit

--- 222.py
+++ 222.py
@@ -1,5 +1,9 @@
-country = input()
-x = country.split()
-for word in x:
-    print(word)
-
+wives = 7
+bag = 7
+bags = wives * bag
+cat = 7
+cats = bags * cat
+kitty = 7
+kitties = cats * kitty
+total = 1 + 1 + wives + cats + kitties
+print(total)
