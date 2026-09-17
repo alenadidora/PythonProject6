82d65a42db5ab18cc51b0b40673828455f5d66d1
alenadidora <a.didora@g.nsu.ru>
2026-09-17 23:18:47 +0700

    33 commit

--- 222.py
+++ 222.py
@@ -1,5 +1,11 @@
-n = float(input())
-total_minutes = int(n // 0.5)
-hours = total_minutes // 60
-minutes = total_minutes % 60
-print(f"{hours} {minutes}")
+n = int(input())
+c = int(input())
+k = int(input())
+
+per_page = n * c
+page = (k - 1) // per_page + 1
+pos = (k - 1) % per_page + 1
+col = (pos - 1) // n + 1
+row = (pos - 1) % n + 1
+
+print(f"страница {page} столбец {col} строка {row}")
