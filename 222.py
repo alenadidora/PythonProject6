dcc669643060f5f2b32682d8dd9ccbf7b78542a8
alenadidora <a.didora@g.nsu.ru>
2026-09-17 23:02:02 +0700

    29 commit

--- 222.py
+++ 222.py
@@ -1,7 +1,13 @@
-raw = input('Enter number: ')
+import math
 
-try:
-    num = int(raw)
-    print(num)
-except ValueError:
-    print('Ошибка: введено не число')
+a = float(input())
+b = float(input())
+c = float(input())
+
+A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+B = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+C = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+
+print(A)
+print(B)
+print(C)
