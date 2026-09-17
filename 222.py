b3b00bcd6c3b162a4ed955e339263780055e7f34
alenadidora <a.didora@g.nsu.ru>
2026-09-17 22:35:57 +0700

    23 commit

--- 222.py
+++ 222.py
@@ -1,3 +1,5 @@
-price = int(input())
-x = (price // 100) % 10
-print(x)
+total_seconds = int(input())
+hours = total_seconds // 3600
+minutes = (total_seconds % 3600) // 60
+seconds = total_seconds % 60
+print(f"{hours} часов {minutes} минут {seconds} секунд")
