ccf7fb99aff943df8680f2bc5c5166ea49101409
alenadidora <a.didora@g.nsu.ru>
2026-09-17 22:37:08 +0700

    24 commit

--- 222.py
+++ 222.py
@@ -1,5 +1,2 @@
-total_seconds = int(input())
-hours = total_seconds // 3600
-minutes = (total_seconds % 3600) // 60
-seconds = total_seconds % 60
-print(f"{hours} часов {minutes} минут {seconds} секунд")
+n = int(input())
+print(n % 2)
